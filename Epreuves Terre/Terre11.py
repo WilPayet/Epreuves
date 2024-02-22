fe7ccc1499abdc7fc9f362e8cae5902c887a381a
WilPayet <127959396+WilPayet@users.noreply.github.com>
2024-02-22 20:40:42 +0100

    Update Terre11.py

--- Epreuves Terre/Terre11.py
+++ Epreuves Terre/Terre11.py
@@ -1,10 +1,26 @@
-import time
 import sys
 
-heure = sys.argv[1]
+if len(sys.argv) != 2:
+	print("Erreur")
+else:
+	time = sys.argv[1]
+	res = time.split(":")  #séparer l'heure en deux éléments
+if len(res) == 2:
+	hour = int(res[0])
+	minute = int(res[1])
 
-format = time.strptime(heure, '%H:%M')
-
-heure_pm = time.strftime('%I:%M %p', format)
-
-print(heure_pm)
+	if hour > 12:
+		hour_12 = hour - 12
+		time_period = "PM"
+	elif hour == 12:
+		hour_12 = 12
+		time_period = "PM"
+	elif hour == 00:
+		hour_12 = 12
+		time_period = "AM"
+	else:
+		hour_12 = hour
+		time_period = "AM"
+	print(hour_12,':', minute, time_period)
+else:
+	print("Erreur: Veuillez utiliser le format 00:00")
