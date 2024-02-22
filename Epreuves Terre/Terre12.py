ab5812ba86d5020a24e3349187e3c3ac18708801
WilPayet <127959396+WilPayet@users.noreply.github.com>
2024-02-23 00:50:20 +0100

    Update Terre12.py

--- Epreuves Terre/Terre12.py
+++ Epreuves Terre/Terre12.py
@@ -1,10 +1,33 @@
 import sys
-import time 
 
-heure = sys.argv[1]
+if len(sys.argv) > 7:
+    print("Erreur")
+else:
+    time = sys.argv[1]
+    
 
-format_12h = time.strptime(heure, '%I:%M%p')
+    if len(time) == 7:
+        hour = time[0:2]
+        minute = time[3:5]
+        period = time[5:]
 
-heure_24h = time.strftime('%H:%M', format_12h)
 
-print(heure_24h)
+        if hour.isdigit() and minute.isdigit():
+            hour12 = int(hour)
+            minute12 = int(minute)
+
+            if hour12 <= 12 and minute12 <= 59:
+                if hour12 < 12 and period == 'PM':
+                    hour24 = hour12 + 12
+                else:
+                    hour24 = hour12
+                if hour12 == 12 and period == 'AM':
+                    hour24 = hour12 - 12
+
+                    print(hour24, ':', minute)
+            else:
+                print('Erreur: veuillez utiliser le format 00:00AM/PM')
+        else:
+            print("Erreur: veuillez utiliser des chiffres")
+    else:
+        print('Erreur: veuillez utiliser le format 00:00AM/PM')
